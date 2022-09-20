3dd7bd48dcebb4ddb17f0c2045eade9870a59f28
Svetlana Karslioglu <svekars@fb.com>
2022-09-20 13:47:07 -0700

    add last-updated-script

---
+++ last_updated2.sh
@@ -0,0 +1,5 @@
+#!/bin/bas
+# current script
+git ls-tree -r master --name-only | grep -E '.*\.(rst|py)' | while read filename; do
+  echo "$(git log -1 --date=short --format="%ad" -- $filename) $filename";
+done | sort -r | tr  " " ","  > file.csv
