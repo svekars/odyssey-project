ab5ce8278a4352713d9aaca5d22a1f0919c5b4d2
Svetlana Karslioglu <svekars@fb.com>
2022-09-21 14:33:45 -0700

    Update

---
+++ git-history.sh
@@ -0,0 +1,3 @@
+echo "commit id,author,date,title,changed files,lines added,lines deleted" > log.csv 
+git log --date=short --pretty=format:@%h,%an,%ad,%s --shortstat | tr "\n" " " | tr "@" "\n" >> log.csv
+sed -i '' 's/ files changed//g;s/ file changed//g;s/ insertions(+)//g;s/ insertion(+)//g;s/ deletions(-)//g;s/ deletion(-)//g' log.csv
